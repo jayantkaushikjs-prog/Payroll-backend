1c071973676c229bd410598a9390a95810812332
jayantkaushikth <jayant.kaushik@techindustan.com>
2026-07-01 13:18:46 +0530

    imps 11

--- src/modules/employees/employees.service.ts
+++ src/modules/employees/employees.service.ts
@@ -407,8 +407,8 @@ export class EmployeesService {
     const employees = await this.employeesRepository.find();
     let maxNum = 0;
     employees.forEach(emp => {
-      if (emp.employee_code && emp.employee_code.startsWith('EMP')) {
-        const numPart = emp.employee_code.substring(3);
+      if (emp.employee_code && emp.employee_code.startsWith('TS -')) {
+        const numPart = emp.employee_code.substring(4);
         const num = parseInt(numPart, 10);
         if (!isNaN(num) && num > maxNum) {
           maxNum = num;
@@ -416,7 +416,7 @@ export class EmployeesService {
       }
     });
     const nextNum = maxNum + 1;
-    const code = `EMP${String(nextNum).padStart(3, '0')}`;
+    const code = `TS -${String(nextNum).padStart(3, '0')}`;
     return { code };
   }
 
